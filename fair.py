96940da908cbef0a35612fce0fa6f583594db08f
Anna Zhang <annazhangg@yahoo.com>
2021-11-20 23:28:50 -0500

    create bars connecting the two wheels of the ferris wheel and create ground posts for support

--- fair.py
+++ fair.py
@@ -10,8 +10,8 @@ import bmesh
 class Wheel:
     def __init__(self, center, num_carts, size):
         self.center = center # center of wheel, user-specified
-        self.num_carts = num_carts # number of carts, user-specified
-        self.size = size # size of wheel, user-specified
+        self.num_carts = 15 # number of carts, user-specified
+        self.size = 3.0 # size of wheel, user-specified
         
         self.vertices = [] # vertices of the ferris wheel
         self.edges = [] # edges of the ferris wheel
@@ -20,13 +20,15 @@ class Wheel:
         self.wheel2 = {"vertex_indices": {}, "edge_indices": {}}
         self.posts = {"vertex_indices": {}, "edge_indices": {}}
         
-        self.create_wheel((self.center[0], self.center[1] - 2.0, self.center[2]), 3.0, 8, self.wheel1)
-        self.create_wheel((self.center[0], self.center[1] + 2.0, self.center[2]), 3.0, 8, self.wheel2)
+        self.create_wheel((self.center[0], self.center[1] - (0.25 * self.size), self.center[2]), self.size, self.num_carts, self.wheel1) # 2.0 is the radius of a ring holding a cart
+        self.create_wheel((self.center[0], self.center[1] + (0.25 * self.size), self.center[2]), self.size, self.num_carts, self.wheel2)
+        self.connect_wheels()
+        self.create_posts(self.wheel1, 1)
+        self.create_posts(self.wheel2, 2)
     
     def create_wheel(self, center, radius, num_vertices, wheel_dict): # add vertices of circle with center "center" and radius "radius"
             theta = (2 * pi) / num_vertices # calculate angle of each slice of the circle
             circle_center = np.asarray(center)
-            print("here")
             
             self.vertices.append(center) # add vertex for circle center
             wheel_dict["vertex_indices"]["center"] = len(self.vertices) - 1
@@ -65,7 +67,35 @@ class Wheel:
                 self.edges.append([center_vertex, circle_vertices[i]])
                 spoke_edges.append(total_edges + i) # store what index that newly added spoke edge is within the entire edges list
             wheel_dict["edge_indices"]["spoke"] = spoke_edges
-                
+            
+    def connect_wheels(self):
+        wheel1_circle_vertices = self.wheel1["vertex_indices"]["outer_circle"] # get list of numbers corresponding to the indices of the outer circle vertices in the vertices array
+        wheel2_circle_vertices = self.wheel2["vertex_indices"]["outer_circle"] # get list of numbers corresponding to the indices of the outer circle vertices in the vertices array
+        wheel_rings = [] # hold edges connecting the wheels (the rings the carts are on)
+        total_edges = len(self.edges) # the total number of edges
+        for i in range(self.num_carts):
+            self.edges.append([wheel1_circle_vertices[i], wheel2_circle_vertices[i]]) # create an edge between the corresponding vertices on the two wheels
+            wheel_rings.append(total_edges + i) # store what index that newly added edge is within the entire edges list
+            
+    def create_posts(self, wheel, wheel_num):
+        # wheel_num is either 1 or 2, depending on which wheel of this ferris wheel is specified to create posts for
+        # create the posts supporting this wheel
+        wheel_center_ground = self.vertices[wheel["vertex_indices"]["center"]]
+        wheel_center_ground = (wheel_center_ground[0], wheel_center_ground[1], -1.25 * self.size) # point on ground that has 1.25 * radius of the wheel
+        wheel_post1_vertex = (wheel_center_ground[0] - (0.6 * self.size), wheel_center_ground[1], wheel_center_ground[2]) # the left post supporting the wheel, the point on ground that has 1.5 * radius of the wheel and that is to the left of the center vertex
+        wheel_post2_vertex = (wheel_center_ground[0] + (0.6 * self.size), wheel_center_ground[1], wheel_center_ground[2]) # the right post supporting the wheel, the point on ground that has 1.5 * radius of the wheel and that is to the right of the center vertex
+        # these vertices are support points for the wheel
+        total_vertices = len(self.vertices)
+        self.vertices.append(wheel_post1_vertex)
+        self.vertices.append(wheel_post2_vertex)
+        self.posts["vertex_indices"]["wheel" + str(wheel_num)] = [total_vertices, total_vertices + 1]
+        
+        # create edges between the center of the wheel and the post support points on the ground
+        total_edges = len(self.edges)
+        self.edges.append([wheel["vertex_indices"]["center"], total_vertices])
+        self.edges.append([wheel["vertex_indices"]["center"], total_vertices + 1])
+        self.posts["edge_indices"]["wheel" + str(wheel_num)] = [total_edges, total_edges + 1]
+            
                 
  
 # test
